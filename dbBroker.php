bc34cb3103abd5bbde8437b696d743e0952476ae
Irena Zivkovic <zivkovicirena3@gmail.com>
2022-11-17 15:29:49 +0100

    baza

---
+++ dbBroker.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$db = "ordinacija";
+$user = "root";
+$pass = "";
+
+$conn = new mysqli($host,$user,$pass,$db);
+
+if ($conn->connect_errno) {
+    exit("Neuspesna konekcija: greska> ". $conn->connect_error.", err kod>".$conn->connect_errno);
+}
+?>
